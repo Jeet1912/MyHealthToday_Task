1f03f71a240126b6fb04b85a1539c2340a07bbc8
Jeet1912 <51080094+Jeet1912@users.noreply.github.com>
2020-11-20 20:59:57 +0530

    UI done

---
+++ lib/ui/facts_card.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FactsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 493,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Color(0xFFF6F6F6),
+      ),
+      padding: EdgeInsets.symmetric(vertical: 27),
+      child: Text("Facts"),
+    );
+  }
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:task/ui/facts_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE5E5E5),
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 54.0,
+        ),
+        child: ListView(children: [
+          _createTopPortion(),
+          Center(
+              child: Padding(
+            padding: EdgeInsets.only(
+              left: 141.0,
+              right: 227.0,
+              top: 113.0,
+            ),
+            child: _createGridView(context),
+          )),
+        ]),
+      ),
+    );
+  }
+
+  Widget _createTopPortion() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Image.network(
+          "https://bestofmycat.com/wp-content/uploads/2019/08/cat-facts-800x445.png",
+          height: 361,
+          width: 670,
+        ),
+        SizedBox(
+          height: 21,
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xFFFFF8F8),
+              ),
+              width: 518,
+              child: TextField(
+                textAlign: TextAlign.center,
+                autofocus: false,
+                decoration: InputDecoration(
+                  hintText: "Enter Numer of Facts you want",
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: 18,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF9A9797),
+                      width: 1.0,
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Color(0xFF9A9797), width: 1.0),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 24,
+            ),
+            FlatButton(
+              height: 52,
+              minWidth: 116,
+              color: Color(0xFFC4C4C4),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              onPressed: () {},
+              child: Text(
+                "Get Facts",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _createGridView(BuildContext context) {
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 21,
+        crossAxisSpacing: 86,
+      ),
+      itemBuilder: (context, index) {
+        return FactsCard();
+      },
+      itemCount: 4,
+    );
+  }
+}
